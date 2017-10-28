07e4182f9c8d156a78d9fed7956ac2640c3a2031
Alexandru Cristu <alexandru.cristu@computaris.com>
2017-10-28 11:12:27 +0300

    goal for next week's hackaton: complete the main demo flow

--- src/app/test/main_demo_flow.ts
+++ src/app/test/main_demo_flow.ts
@@ -10,8 +10,12 @@ export const SETUP = {
     initialEntities: new MockMetadata().entities
 };
 
-export const FLOW = {
+let SYSTEMNAME = 'Cloudev'; //NEED IDEAS HERE!!! metawiz is not catchy enough, cloudev sound better by there is a cloudevtech.com, let's open a poll
 
+export const FLOW = {
+    comment1: `The 5 min overview of ${SYSTEMNAME}
+        System running off existing metadata and data.
+        Nothing really special, just standard looking ERP software`,
     When_user_first_accesses_the_app: {},
     Then_navigation_should_show_all_current_tables: {
         initialEntitiesChangesAction: new navState.EntitiesChangesAction(SETUP.initialEntities.map(e => new ChangeObj(e)))
@@ -22,5 +26,16 @@ export const FLOW = {
     When_user_navigates_to_a_service_form: {},
     Then_the_form_page_should_be_displayed: {},
     When_user_updates_the_requested_quantity_of_a_product_list_item: {},
-    Then_user_will_see_the_reserved_quantity_and_stock_computed_by_the_engine: {}
+    Then_user_will_see_the_reserved_quantity_and_stock_computed_by_the_engine: {},
+    TODO_user_creates_new_service_form: {},
+
+    comment2: `What is special about ${SYSTEMNAME}: this standard looking ERP software is implemented using a simple language accesible to busines people`,
+    TODO_use_editor_to_change_order_of_columns_in_table: {},
+    TOOD_use_editor_to_change_form_layout: {},
+
+    comment3: `So far only simple changes in the ui layout, let's see how easy it is to add new functionality to the system.
+        We argue it is as simple as editing a Spreadsheet/Excel, anybody who has used Excel with a few formulas can use ${SYSTEMNAME}.`,
+    TODO_create_Revision_Entity: {},
+    TOOD_create_Revision_Form: {},
+    TODO_create_and_edit_revisions: {},
 };
