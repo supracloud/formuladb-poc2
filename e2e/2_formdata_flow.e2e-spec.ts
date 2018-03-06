868ca42c5f1cd0a3fb2948761ed45a80f490df33
Sorin Cirstoiu <carstos@gmail.com>
2018-03-07 00:18:07 +0200

    updated 2_formdata_flow.e2e-spec.ts

---
+++ e2e/2_formdata_flow.e2e-spec.ts
@@ -0,0 +1,57 @@
+import { AppPage } from './app.po';
+import { NavigationPO } from './navigation.po';
+import { TablePO } from "./table.po";
+import { browser, by, element, promise, Browser } from 'protractor';
+import { $wait } from "./common";
+import { ElementFinder } from 'protractor/built/element';
+
+describe('2_formdata_flow: ', () => {
+  let appPage: AppPage;
+  let navPO: NavigationPO;
+  let tablePO: TablePO;
+
+  beforeEach(() => {
+    appPage = new AppPage();
+    navPO = new NavigationPO();
+    tablePO = new TablePO();
+  });
+
+  it('User should be able to change the name property for an /General/Actor entity', async () => {
+    // hack required in order to locate elements on the page
+    browser.ignoreSynchronization = true;
+
+    await appPage.rootPage();
+    
+    // navigate to Actor entity page
+    await navPO.navToEntityPage('/General/Actor', 'Actor');
+
+    // save original values
+    let tableContents = await tablePO.getTable();
+
+    let actorName = tableContents[1][3];
+    let actorNameUpdated = actorName + 'New';
+
+    let firstCell = await tablePO.firstCell();
+    
+    // open service form in edit mode by double clicking on the table entry
+    browser.actions().doubleClick(firstCell).perform();
+
+    // update the actor name to actorNameUpdated
+    navPO.fieldActorName().clear().then(function() {
+      navPO.fieldActorName().sendKeys(actorNameUpdated);
+    });
+
+    browser.enterRepl();
+
+    // navigate back to Actor entity page
+    await navPO.navToEntityPage('/General/Actor', 'Actor');
+
+    tableContents = await tablePO.getTable();
+
+    console.log(tableContents[1][3]);
+
+    // check that name property changed for the first Actor entity
+    expect(tableContents[1][3]).toEqual(actorNameUpdated);
+  });
+
+});
--- e2e/navigation.po.ts
+++ e2e/navigation.po.ts
@@ -1,8 +1,18 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 import { $wait } from "./common";
 
 export class NavigationPO {
   navLinkForEntity(entityPath: string) {
     return $wait(element(by.css(`[href="${entityPath}"]`)));
   }
-}
+
+  async navToEntityPage(entityPath: string, entityName: string) {
+    let link = await this.navLinkForEntity(entityPath);
+    await expect(link.getText()).toContain(entityName);
+    await link.click();
+  }
+
+  fieldActorName(): ElementFinder {
+    return element(by.name('name'));
+  }
+} 
