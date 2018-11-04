6c33cddc2e4a703b7b16bfbd7efaf4e2c497f106
Alexandru Cristu <alexandru.cristu@computaris.com>
2018-11-04 12:43:06 +0200

    more test data

--- misc/generate-mock-data.js
+++ misc/generate-mock-data.js
@@ -1,46 +1,59 @@
 var faker = require('faker');
-var fs = require('fs');
 
 let jsonArr = [];
 let objs = {};
 
 const UserRoles = ['ADMIN', 'USER'];
 
-function g(count, callback) {
-    for (let i = 1; i <= count; i++) {
-        let obj = callback(i);
-        jsonArr.push(obj);
-        objs[obj._id] = obj;
+function generateActors() {
+    let ret = [];
+    for (let i = 1; i <= 10; i++) {
+        var firstName = faker.name.firstName(),
+            lastName = faker.name.lastName();
+
+        ret.push({
+            _id: "General___Actor~~" + i,
+            firstName: firstName,
+            lastName: lastName,
+            username: faker.internet.userName(firstName, lastName),
+            email: faker.internet.email(firstName, lastName),
+            avatar: faker.internet.avatar(), 
+            name: faker.name.findName(firstName, lastName), 
+            role: UserRoles[i] || 'USER', 
+            password: faker.internet.password(),
+            details: faker.name.title(),
+            state: "ACTIVE_" 
+        });
     }
+    return ret;
 }
 
-g(5, i => ({_id: "General___Actor~~" + i, code: "act" + i, username: faker.internet.userName(), avatar: faker.internet.userName(), name: faker.name.findName(), role: UserRoles[i] || 'USER',  password: faker.internet.password(),  details: faker.name.title(),  state: "ACTIVE_" }));
-g(1, i => ({_id: "General___Currency~~1", "rate1": 101,"rate2": 102,"rate3": 103,"rate4": 104,"rate5": 105}));
-g(1, i => ({_id: "General___Person~~1"}));
-g(1, i => ({_id: "General___User~~1"}));
-g(1, i => ({_id: "General___Client~~1"}));
-g(1, i => ({_id: "Inventory___Product~~1", code: "p1", barcode: "11111111", name: "Product1", description: "Product 1 Description lorem ipsum bla bla"}));
-g(1, i => ({_id: "Inventory___Product___Location~~1___1", locationCode: "Warehouse1", category: "C1", price: 12.5,received_stock__: 15, ordered_stock__: 14, available_stock__: 1}));
-g(1, i => ({_id: "Inventory___ProductUnit~~1"}));
-g(1, i => ({_id: "Inventory___Order~~1", sales_agent: "John Doe", creation_date: "2018-09-27"}));
-g(1, i => ({_id: "Inventory___Order___Item~~1___1", Inventory___Product___Location$product: {_id: "Inventory___Product___Location~~1___1"}, quantity: 10}));
-g(1, i => ({_id: "Inventory___Order___Item~~1___2", Inventory___Product___Location$product: {_id: "Inventory___Product___Location~~1___1"}, quantity: 4}));
-g(1, i => ({_id: "Inventory___Receipt~~1"}));
-g(1, i => ({_id: "Inventory___Receipt___Item~~1___1", Inventory___Product___Location$product: {_id: "Inventory___Product___Location~~1___1"}, quantity: 10}));
-g(1, i => ({_id: "Inventory___Receipt___Item~~1___2", Inventory___Product___Location$product: {_id: "Inventory___Product___Location~~1___1"}, quantity: 5}));
-g(1, i => ({_id: "Forms___ServiceForm~~1"}));
-g(1, i => ({_id: "Reports___DetailedCentralizerReport~~1"}));
-g(1, i => ({_id: "Reports___ServiceCentralizerReport~~1"}));
-
-fs.writeFile("../fe/src/app/common/test/mocks/faker-mock-data.ts", 
-    "export const FAKER_MOCK_DATA = [\n"
-    + jsonArr.map(x => JSON.stringify(x)).join(",\n")
-    + "\n];\n",
-    function(err) {
-        if(err) {
-            return console.log(err);
-        }
-
-        console.log("The file was saved!");
+function generateCompany() {
+    let ret = [];
+    for (let i = 1; i <= 10; i++) {
+        ret.push({
+            _id: "General___Client~~" + i,
+            name: faker.company.companyName(),
+            email: faker.internet.email(),
+            bs: faker.company.bs(), 
+            catchPhrase: faker.company.catchPhrase(), 
+        });
     }
-);
+    return ret;
+}
+
+function generateProducts() {
+    let ret = [];
+    for (let i = 1; i <= 25; i++) {
+        ret.push({ _id: "Inventory___Product~~"+i, code: "p" + i, barcode: "400638133393" + i, 
+            name: faker.commerce.product(), 
+            category: faker.commerce.department(), 
+            subcategory: faker.lorem.word(), 
+            description: faker.lorem.sentence(),
+            longDescription: faker.lorem.paragraphs()
+        });
+    }
+    return ret;
+}
+
+console.info(generateProducts().map(x => JSON.stringify(x)).join(",\n"));
