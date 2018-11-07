83f48ab0ca39287d0394d0dcd84e815061de0ec6
Laurentiu Soica <laurentiu.soica@viavisolutions.com>
2018-11-07 18:17:59 +0200

    just for sync

--- fe/e2e/src/app.e2e-spec.ts
+++ fe/e2e/src/app.e2e-spec.ts
@@ -3,18 +3,51 @@
  * License TBD
  */
 
-import { AppPage } from './app.po';
-import { browser } from 'protractor';
+import { browser, element, ExpectedConditions, by } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
-
+  var until = ExpectedConditions;
   beforeEach(() => {
-    page = new AppPage();
+    browser.ignoreSynchronization = true;
+    browser.driver.manage().window().maximize();  });
+
+  it('should display main page', () => {
+    browser.driver.get('http://localhost:4200/');
+    expect(browser.getTitle()).toEqual('Fe');
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('BRAND');
+  it('Go to Financial page', () => {
+    var financial = element(by.css('[href="/0/Financial"]'));
+    browser.wait(until.presenceOf(financial), 50000, 'Element taking too long to appear in the DOM');
+    financial.click();
+    browser.sleep(2000);
   });
+
+  it('Go to Forms page', () => {
+    var forms = element(by.css('[href="/0/Forms"]'));
+    browser.wait(until.presenceOf(forms), 50000, 'Element taking too long to appear in the DOM');
+    forms.click();
+    browser.sleep(2000);
+  })
+
+  it('Go to General page', () => {
+    var general = element(by.css('[href="/0/General"]'));
+    browser.wait(until.presenceOf(general), 50000, 'Element taking too long to appear in the DOM');
+    general.click();
+    browser.sleep(2000);
+  })
+
+  it('Go to Inventory page', () => {
+    var inventory = element(by.css('[href="/0/Inventory"]'));
+    browser.wait(until.presenceOf(inventory), 50000, 'Element taking too long to appear in the DOM');
+    inventory.click();
+    browser.sleep(2000);
+  })
+
+  it('Go to Reports page', () => {
+    var reports = element(by.css('[href="/0/Reports"]'));
+    browser.wait(until.presenceOf(reports), 50000, 'Element taking too long to appear in the DOM');
+    reports.click();
+    browser.sleep(2000);
+  })
 });
