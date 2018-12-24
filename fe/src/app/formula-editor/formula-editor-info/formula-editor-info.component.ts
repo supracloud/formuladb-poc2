7918cf6baf75335bdf3536111b7dd4f01955d96f
Gabriel <deroude@gmail.com>
2018-12-24 10:56:32 +0200

    editor drag

--- fe/src/app/formula-editor/formula-editor-info/formula-editor-info.component.ts
+++ fe/src/app/formula-editor/formula-editor-info/formula-editor-info.component.ts
@@ -12,11 +12,27 @@ export class FormulaEditorInfoComponent implements OnInit {
 
   public editorOpened$: Observable<boolean>;
 
+  x: number = 50;
+  y: number = 50;
+  deltaX: number = 0;
+  deltaY: number = 0;
+
   constructor(private formulaEditorService: FormulaEditorService) {
     this.editorOpened$ = formulaEditorService.editorExpr$.pipe(map(editorTxt => editorTxt != undefined));
   }
 
   ngOnInit() {
+    window.addEventListener('dragover', e => e.preventDefault());
   }
 
+  dragHandle(e: any) {
+    console.log(e);
+    this.x = e.clientX - this.deltaX;
+    this.y = e.clientY - this.deltaY;
+  }
+
+  dragStartHandle(e: any) {
+    this.deltaX = e.clientX - this.x;
+    this.deltaY = e.clientY - this.y;
+  }
 }
