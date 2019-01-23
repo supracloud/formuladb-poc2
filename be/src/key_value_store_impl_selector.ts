a698b15d3481d0125b46655fa398308dc16dfcd7
Laurentiu Soica <laurentiu.soica@viavisolutions.com>
2019-01-23 18:48:13 +0200

    sync

--- be/src/key_value_store_impl_selector.ts
+++ be/src/key_value_store_impl_selector.ts
@@ -5,6 +5,7 @@
 
 import { KeyValueStoreFactoryI } from "@storage/key_value_store_i";
 import { KeyValueStoreFactoryPostgres } from "@storage/postgres/key_value_store_postgres";
+import { KeyValueStoreFactoryMem } from "@storage/mem/key_value_store_mem";
 
 let KeyValueStoreFactory: KeyValueStoreFactoryI;
 KeyValueStoreFactory = new KeyValueStoreFactoryPostgres()
--- storage/src/postgres/key_value_store_postgres.ts
+++ storage/src/postgres/key_value_store_postgres.ts
@@ -10,6 +10,8 @@ import * as _ from "lodash";
 import { KeyValueObj, KeyValueError } from "../domain/key_value_obj";
 import * as pgPromise from "pg-promise";
 import * as dotenv from "dotenv";
+// TO BE UPDATED: tables must be reused not recreated on each app restart
+const { uniqueNamesGenerator } = require('unique-names-generator');
 
 /**
  * Key Value Store with optimistic locking functionality
@@ -17,6 +19,7 @@ import * as dotenv from "dotenv";
 export class KeyValueStorePostgres<VALUET> implements KeyValueStoreI<VALUET> {
 
     static db:pgPromise.IDatabase<any>|undefined = undefined;
+    private table_id:string|undefined = undefined;
 
     constructor() {
         dotenv.config();
@@ -31,34 +34,51 @@ export class KeyValueStorePostgres<VALUET> implements KeyValueStoreI<VALUET> {
         }
     }
 
-    public get(_id: string): Promise<VALUET> {
-        return new Promise(async (resolve) => {
+    private async initialize() {
+        if (this.table_id == undefined) {
+            this.table_id = uniqueNamesGenerator();
+
+            let query: string = 'DROP TABLE ' + this.table_id;
             try {
-                const res = await KeyValueStorePostgres.db!.oneOrNone<VALUET>('SELECT val FROM kvtable WHERE key = $1', [_id] );            
-                resolve(res != null ? res : undefined);
-            } catch ( err ) {
-                console.log( err );
-                throw err;
-            }
+                await KeyValueStorePostgres.db!.any(query);
+            } catch(err) { }
+            query = 'CREATE TABLE ' + this.table_id +' (key VARCHAR NOT NULL PRIMARY KEY, val json)';
+            await KeyValueStorePostgres.db!.any(query);
+        }
+    }
+
+    public get(_id: string): Promise<VALUET> {
+        return new Promise((resolve) => {
+            this.initialize().then(() => {
+                console.log("get ", _id);
+                let query: string = 'SELECT val FROM ' + this.table_id + ' WHERE key = $1' ;
+
+                KeyValueStorePostgres.db!.oneOrNone<VALUET>(query, [_id] ).then((res) => {
+                    // Another issue here: res comes as JSON/object
+                    resolve(res != null ? res : undefined);
+                })
+            })
         });
     }
 
     /** querying a map-reduce view must return the results ordered by key */
     public rangeQueryWithKeys(opts: RangeQueryOptsI): Promise<{key: string, val: VALUET}[]> {
-        return new Promise(async (resolve) => {
-            let sign1: string = opts.inclusive_start ? ">=" : ">";
-            let sign2: string = opts.inclusive_end ? "<=" : "<";
-            let start: string = opts.startkey;
-            let end: string = opts.endkey;
-            try {
-                let query: string = 'SELECT key, val FROM kvtable WHERE key ' + sign1 + ' $1 and key ' + sign2 + ' $2' + ' ORDER BY key';
-                const res = await KeyValueStorePostgres.db!.any<{key: string, val: VALUET}>(query, [ start, end ] );            
-                resolve(res);
-            } catch ( err ) {
-                console.log( err );
-                throw err;
-            }
-        });
+        return new Promise((resolve, reject) => {
+            this.initialize().then(() => {
+                let sign1: string = opts.inclusive_start ? ">=" : ">";
+                let sign2: string = opts.inclusive_end ? "<=" : "<";
+                let start: string = opts.startkey;
+                let end: string = opts.endkey;
+                // ISSUE here: cannot handle unicode in select
+                end = end.replace(/[\ufff0]/g,'\xff');
+                let query: string = 'SELECT key, val FROM ' + this.table_id + ' WHERE key ' + sign1 + ' $1 AND key ' + sign2 + ' $2 ' + ' ORDER BY key';
+                KeyValueStorePostgres.db!.any<{key: string, val: VALUET}>(query, [start, end]).then((res) => {
+                    resolve(res);
+                }).catch((err) => {
+                    console.log(err.message);
+                })
+            })
+        })
     }
 
     public rangeQuery(opts: RangeQueryOptsI): Promise<VALUET[]> {
@@ -67,24 +87,24 @@ export class KeyValueStorePostgres<VALUET> implements KeyValueStoreI<VALUET> {
     }
 
     public set(_id: string, obj: VALUET): Promise<VALUET> {
-        return new Promise(async (resolve) => {
-            try {
-
+        return new Promise((resolve) => {
+            this.initialize().then(() => {
                 let object_as_json = JSON.stringify(obj);
-                await KeyValueStorePostgres.db!.none("INSERT INTO kvtable VALUES($1, $2) ON CONFLICT (key) DO UPDATE SET key=$1, val=$2", [_id, object_as_json]);
-                resolve(obj);
-            } catch ( err ) {
-                console.log( err );
-                throw err;
-            }
+                let query: string = 'INSERT INTO ' + this.table_id + ' VALUES($1, $2) ON CONFLICT (key) DO UPDATE SET key=$1, val=$2' ;
+                KeyValueStorePostgres.db!.none(query, [_id, object_as_json]).then((res) => {
+                    resolve(obj);
+                })
+            })
         })
     }
 
     public async del(_id: string): Promise<VALUET> {
 
+        await this.initialize();
         try {
             let ret = await this.get(_id);
-            await KeyValueStorePostgres.db!.none('DELETE FROM kvtable WHERE  key = $1', [ _id ]);
+            let query: string = 'DELETE FROM ' + this.table_id + ' WHERE  key = $1' ;
+            await KeyValueStorePostgres.db!.none(query, [_id ]);
             return ret;
         } catch ( err ) {
             console.log( err );
@@ -94,12 +114,14 @@ export class KeyValueStorePostgres<VALUET> implements KeyValueStoreI<VALUET> {
     }
 
     public async clearDB() {
-        await KeyValueStorePostgres.db!.any(`TRUNCATE TABLE kvtable`);
+        await this.initialize();
+        let query: string = 'TRUNCATE TABLE ' + this.table_id;
+        await KeyValueStorePostgres.db!.any(query);
     }
 
     public info(): Promise<string> {
         return new Promise(async (resolve) => {
-            resolve("in memory test KVS");
+            resolve("Postgres based KVS");
         })
     }
 }
