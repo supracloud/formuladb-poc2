0fb380520f60be82518464fcad24814bac61cb3d
Laurentiu Soica <laurentiu@soica.ro>
2019-01-28 09:20:41 +0000

    Fix mount volume on linux

--- storage/bin/start_postgres.js
+++ storage/bin/start_postgres.js
@@ -2,7 +2,7 @@
 var shell = require('shelljs');
 
 if (!shell.which('docker')) {
-    shell.echo('Sorry, this script requires git');
+    shell.echo('Sorry, this script requires docker');
     shell.exit(1);
 } else {
     shell.echo('Docker found. Starting postgres container ...')
@@ -13,12 +13,12 @@ if (shell.exec('docker inspect --format="{{ .State.Status }}" formuladb-pg').cod
     shell.echo('formuladb-pg container already present. Starting it, just in case ...');
     shell.exec('docker start formuladb-pg');
 
-} else if (shell.exec('docker run -d --name formuladb-pg -p 5432:5432 -v ./postgres-data:/var/lib/postgresql/data -e \'POSTGRES_PASSWORD=p@ssw0rd42\' postgres:latest').code !== 0) {
+} else if (shell.exec('docker run -d --name formuladb-pg -p 5432:5432 --mount source=pg_data,target=/var/lib/postgresql/data -e \'POSTGRES_PASSWORD=p@ssw0rd42\' postgres:latest').code !== 0) {
 
         shell.echo('Error: Failed to start Postgres container');
         shell.exit(1);
 } else {
-    shell.echo('Successfuly started FormulaDB potgres container');
+    shell.echo('Successfuly started FormulaDB postgres container');
 }
 
 shell.exit(0);
