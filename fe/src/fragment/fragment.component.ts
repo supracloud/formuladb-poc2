fff367f023874e4808fbab3a3f8a28c818d2df9a
Alex Cristu <cristualexandru@gmail.com>
2019-07-27 11:13:03 +0300

    fragments are resndered in Shadow DOM so they cannot be modified inside importing pages

--- fe/src/fragment/fragment.component.ts
+++ fe/src/fragment/fragment.component.ts
@@ -27,9 +27,23 @@ export interface FragmentComponentState extends FragmentComponentAttr {
     tag: 'frmdb-fragment',
     observedAttributes: ['name'],
     template: ' ',
-    noShadow: true,
+    style: `
+        :host { display: block }
+    `
 })
 export class FragmentComponent extends FrmdbElementBase<FragmentComponentAttr, FragmentComponentState> {
+    importHTML(html: string) {
+        const template = document.createElement('template');
+        template.innerHTML = html;
+        //FIXME: DOMPurify.sanitize(html) removes <link rel="stylesheet", we need to manually use the purifier and add back the stylesheet
+        const clone = document.importNode(template.content, true);
+        if (!this.shadowRoot) {
+            this.attachShadow({ mode: 'open' });
+        }
+        let el: ShadowRoot = this.shadowRoot!;
+        while (el.firstChild) el.removeChild(el.firstChild);
+        el.appendChild(clone);
+    }
 
     async frmdbPropertyChangedCallback<T extends keyof FragmentComponentState>(attrName: T, oldVal: FragmentComponentState[T], newVal: FragmentComponentState[T]): Promise<Partial<FragmentComponentState>> {
         if (attrName === "name") {
@@ -42,7 +56,7 @@ export class FragmentComponent extends FrmdbElementBase<FragmentComponentAttr, F
             fetch(`/${appBackend.tenantName}/${appBackend.appName}/${fragmentPage.html}`)
             .then(async (response) => {
                 let html = await response.text();
-                this.innerHTML = DOMPurify.sanitize(html);
+                this.importHTML(html);
             });
             
             return this.frmdbState;
