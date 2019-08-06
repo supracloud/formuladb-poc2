bf9c1b417fc9f733f9a020aae821763b1b3d6943
acristu <alexandru.cristu@formuladb.io>
2019-08-06 11:19:36 +0300

    moved vvvebjs into mono-repo

--- serve.sh
+++ serve.sh
@@ -26,8 +26,7 @@ sleep 2
 mkdir -p live-server-wwwroot
 cd live-server-wwwroot #optimize speed
 live-server --wait=200 --port=8081 -V --no-browser \
-    --mount=/formuladb-editor/:../../VvvebJs/ \
-    --mount=/customize-editor.js:./../fe/js/customize-editor.js \
+    --mount=/formuladb-editor/:../vvvebjs/ \
     --mount=/formuladb/frmdb-fe.js:./../dist-fe/frmdb-fe.js \
     --mount=/formuladb/frmdb-fe.js.map:./../dist-fe/frmdb-fe.js.map \
     --mount=/formuladb/frmdb-data-grid.js:./../dist-fe/frmdb-data-grid.js \
