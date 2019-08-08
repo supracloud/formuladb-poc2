caabb13802b316bbb587d4344313908e5d9211ce
Alex Cristu <alexandru.cristu@computaris.com>
2019-08-08 13:30:22 +0300

    bring back assets from gcloud instead of gitlab

--- serve.sh
+++ serve.sh
@@ -12,22 +12,16 @@ fctCheckDir() {
   test -d $1 || (echo "Dir not found ${1}"; exit 1) 
 }
 
-FRMDB_RELEASE=0.0.12 DEV_MODE=true DEFAULT_USER=$USER nodemon --verbose --delay 200ms --watch dist-be/frmdb-be.js --exec \
+FRMDB_RELEASE=0.0.12 DEV_MODE=true DEFAULT_USER=$USER ./node_modules/.bin/nodemon --verbose --delay 200ms --watch dist-be/frmdb-be.js --exec \
   "npm run docker:be && docker-compose up -d db be && nc -zvw3 localhost 8084 && touch dist-fe/frmdb-fe.js" &
 
 sleep 2
 
 mkdir -p live-server-wwwroot
 cd live-server-wwwroot #optimize speed
-live-server --wait=200 --port=8081 -V --no-browser \
+../node_modules/.bin/live-server --wait=200 --port=8081 -V --no-browser \
     --mount=/formuladb-editor/:../vvvebjs/ \
-    --mount=/formuladb/frmdb-fe.js:./../dist-fe/frmdb-fe.js \
-    --mount=/formuladb/frmdb-fe.js.map:./../dist-fe/frmdb-fe.js.map \
-    --mount=/formuladb/frmdb-data-grid.js:./../dist-fe/frmdb-data-grid.js \
-    --mount=/formuladb/frmdb-data-grid.js.map:./../dist-fe/frmdb-data-grid.js.map \
-    --mount=/formuladb/frmdb-form.js:./../dist-fe/frmdb-form.js \
-    --mount=/formuladb/frmdb-editor.js:./../dist-fe/frmdb-editor.js \
-    --mount=/formuladb/frmdb-editor.js.map:./../dist-fe/frmdb-editor.js.map \
+    --mount=/formuladb/:./../dist-fe/ \
     --mount=/frmdb-apps/:../../../frmdb-apps/ \
     --mount=/frmdb-apps/hotel-booking:../../../frmdb-themes/royal-master \
     --mount=/frmdb-apps/inventory:../../../frmdb-themes/startbootstrap-sb-admin-2 \
