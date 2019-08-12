bbb0466cdca64330d0ad7369393fdb8df67da867
Sorin Cirstoiu <carstos@gmail.com>
2019-08-13 00:32:14 +0300

    changed e2e tests location and configuration

---
+++ e2e/src/po/hotel-booking.po.ts
@@ -0,0 +1,16 @@
+import {browser, element, by, ExpectedConditions} from 'protractor';
+
+export class HotelBooking {
+  //inventory = element(by.linkText('Basic Inventory, Single Warehouse'));
+  
+  async navigateToHome() {
+    await browser.get('formuladb-editor/editor.html?t=frmdb-apps&a=hotel-booking&p=index');
+  }
+
+  async getTitle() {
+    // wait for iframe to be loaded
+    browser.wait(ExpectedConditions.presenceOf(element(by.tagName('iframe'))), 5000);
+    browser.switchTo().frame(element(by.id('iframe1')));
+    return element(by.css('h2')).getText();
+  }
+}
