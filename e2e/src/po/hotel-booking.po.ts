2d71a40cc1d0c831fb3e4c5fe94d561509470678
Sorin Cirstoiu <carstos@gmail.com>
2019-08-13 22:44:39 +0300

    updated tests for hotel-booking

--- e2e/src/po/hotel-booking.po.ts
+++ e2e/src/po/hotel-booking.po.ts
@@ -1,8 +1,7 @@
 import {browser, element, by, ExpectedConditions} from 'protractor';
 
 export class HotelBooking {
-  //inventory = element(by.linkText('Basic Inventory, Single Warehouse'));
-  
+ 
   async navigateToHome() {
     await browser.get('formuladb-editor/editor.html?t=frmdb-apps&a=hotel-booking&p=index');
   }
@@ -10,7 +9,9 @@ export class HotelBooking {
   async getTitle() {
     // wait for iframe to be loaded
     browser.wait(ExpectedConditions.presenceOf(element(by.tagName('iframe'))), 5000);
-    browser.switchTo().frame(element(by.id('iframe1')));
+    //browser.switchTo().frame(element(by.id('iframe1')));
+    console.log(await browser.getPageSource());
+    browser.pause();
     return element(by.css('h2')).getText();
   }
 }
