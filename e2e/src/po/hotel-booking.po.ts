46807edf50bbe93301f4d3ab175d1717e87f179a
Sorin Cirstoiu <carstos@gmail.com>
2019-08-13 22:53:28 +0300

    updated tests for hotel-booking

--- e2e/src/po/hotel-booking.po.ts
+++ e2e/src/po/hotel-booking.po.ts
@@ -9,7 +9,7 @@ export class HotelBooking {
   async getTitle() {
     // wait for iframe to be loaded
     browser.wait(ExpectedConditions.presenceOf(element(by.tagName('iframe'))), 5000);
-    //browser.switchTo().frame(element(by.id('iframe1')));
+    browser.switchTo().frame(element(by.id('iframe1')));
     console.log(await browser.getPageSource());
     browser.pause();
     return element(by.css('h2')).getText();
