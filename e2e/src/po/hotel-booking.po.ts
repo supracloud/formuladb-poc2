f824da932be8cba234043f959549e4294773ac7e
Sorin Cirstoiu <sorin.cirstoiu@formuladb.io>
2019-08-14 09:57:39 +0300

    first test available

--- e2e/src/po/hotel-booking.po.ts
+++ e2e/src/po/hotel-booking.po.ts
@@ -7,9 +7,13 @@ export class HotelBooking {
   }
 
   async getTitle() {
+    let EC = ExpectedConditions;
+
     // wait for iframe to be loaded
-    await browser.wait(ExpectedConditions.presenceOf(element(by.css('iframe'))), 5000);
-    await browser.wait(ExpectedConditions.presenceOf(element(by.css('h2'))), 5000);
+    await browser.wait(EC.presenceOf(element(by.css('iframe'))), 5000);
+    await browser.switchTo().frame(0);
+    // wait for the document inside the iframe to be loaded
+    await browser.wait(EC.presenceOf(element(by.css('h2'))), 5000);
     return element(by.css('h2')).getText();
   }
 }
