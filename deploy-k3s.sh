e73b1225c3e9a319ea9704254cf38a5b53086bf1
Laurentiu Soica <laurentiu.soica@formuladb.io>
2019-08-15 12:05:39 +0300

    work on ci

--- deploy-k3s.sh
+++ deploy-k3s.sh
@@ -38,17 +38,3 @@ then
 else
   export KUBECONFIG="$(k3d get-kubeconfig --name='k3s-default')"
 fi
-
-mkdir -p live-server-wwwroot
-cd live-server-wwwroot #optimize speed
-../node_modules/.bin/live-server --wait=200 --port=8081 -V --no-browser \
-    --mount=/formuladb-editor/:../vvvebjs/ \
-    --mount=/formuladb/:./../dist-fe/ \
-    --mount=/formuladb/frmdb-runtime-utils.js:./../fe/js/frmdb-runtime-utils.js \
-    --mount=/frmdb-apps/inventory:../../../frmdb-themes/startbootstrap-sb-admin-2 \
-    --mount=/frmdb-apps/inventory:../../../frmdb-apps/inventory \
-    --mount=/frmdb-apps/hotel-booking:../../../frmdb-apps/hotel-booking \
-    --mount=/frmdb-apps/hotel-booking:../../../frmdb-themes/royal-master \
-    --mount=/:../portal/public \
-    --proxy=/formuladb-api:http://localhost:8084/formuladb-api \
-    --proxy=/:http://localhost:8084/ \
