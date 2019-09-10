7d01c639729b365eea12659fea66d6f0ba4d181b
Laurentiu Soica <laurentiu.soica@formuladb.io>
2019-09-10 09:27:35 +0300

    sync

--- deploy-k3s.sh
+++ deploy-k3s.sh
@@ -9,7 +9,7 @@ handleErr () {
 trap handleErr ERR
 
 #WARNING: This was only tested under WSL.
-#Reqs: k3d, kubectl, skaffold
+#Reqs: k3d, kubectl, skaffold, kustomize (because skaffold cannot yet use kubectl apply -k)
 
 hash kubectl &>/dev/null || { 
   curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
@@ -29,6 +29,18 @@ hash skaffold &>/dev/null || {
   sudo mv skaffold /usr/local/bin
 }
 hash skaffold &>/dev/null || { echo "skaffold not found! See https://skaffold.dev/docs/getting-started/#installing-skaffold"; exit $ERRCODE; }
+
+hash kustomize &>/dev/null || { 
+  curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases/latest |\
+  grep browser_download |\
+  grep linux |\
+  cut -d '"' -f 4 |\
+  xargs curl -O -L
+  mv kustomize_*_linux_amd64 kustomize
+  chmod u+x ./kustomize
+  sudo mv ./kustomize /usr/local/bin/
+}
+hash kustomize &>/dev/null || { echo "kustomize not found! See https://github.com/kubernetes-sigs/kustomize/blob/master/docs/INSTALL.md"; exit 1; }
 
 
 [ -f "$HOME/.docker/config.json" ] || { echo "Not logged in to docker registry. Run docker login registry.gitlab.com"; exit $ERRCODE; }
