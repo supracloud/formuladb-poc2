05ddf7fd98bd387a08e0791af2c18c5012784e29
acristu <alexandru.cristu@formuladb.io>
2019-09-13 15:57:32 +0300

    added hotel-booking app in mono-repo

--- tools/prepare-dev-tenant.sh
+++ tools/prepare-dev-tenant.sh
@@ -1,6 +1,3 @@
-TENANT_NAME=$1
-if [ -z "$TENANT_NAME" ]; then echo "please supply tenant name"; exit 1; fi
-
 export FRMDB_TOOLS_DIR=`dirname $0`
 
 # -------------------------------------------------------------------------
@@ -16,15 +13,4 @@ while ! k3d get-kubeconfig 2>/dev/null; do echo "waiting for k8s ..."; sleep 1; 
 # Second dependency: obj storage
 # -------------------------------------------------------------------------
 
-node $FRMDB_TOOLS_DIR/gcloud.js
-
-# -------------------------------------------------------------------------
-# Third dependency: gitlab
-# -------------------------------------------------------------------------
-
-##TODO
-# curl -v --request PUT --header 'PRIVATE-TOKEN: T8fpbohTXHdsE9yVsL1s' --header "Content-Type: application/json" \
-#     --data '{"name": "'$TENANT_NAME'", "path": "'$TENANT_NAME'", "visibility": "private"}' \
-#     'https://gitlab.com/api/v4/groups'
-
-
+node $FRMDB_TOOLS_DIR/gcloud.js 'createBucketIfNotExists("'$TENANT_NAME'")'
