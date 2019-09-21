d6e62b65253b3bcffb6f66162faf8262f21fa328
Laurentiu Soica <laurentiu@soica.ro>
2019-09-21 14:00:29 +0000

    Download kubectl, shaffold, kustomize

--- ci/prepare-organization.sh
+++ ci/prepare-organization.sh
@@ -6,8 +6,33 @@ export GOOGLE_APPLICATION_CREDENTIALS=$BASEDIR/FormulaDB-storage-full.json
 export KUBECONFIG=$BASEDIR/../k8s/production-kube-config.conf
 
 # -------------------------------------------------------------------------
-# External dependency: obj storage
+# Tooling / Clients
 # -------------------------------------------------------------------------
+hash kubectl &>/dev/null || { 
+  curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+  chmod +x ./kubectl
+  sudo mv ./kubectl /usr/local/bin/kubectl
+}
+hash kubectl &>/dev/null || { echo "kubectl not found! See https://kubernetes.io/docs/tasks/tools/install-kubectl/"; exit 1; }
+
+hash skaffold &>/dev/null || {
+  curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
+  chmod +x skaffold
+  sudo mv skaffold /usr/local/bin
+}
+hash skaffold &>/dev/null || { echo "skaffold not found! See https://skaffold.dev/docs/getting-started/#installing-skaffold"; exit $ERRCODE; }
+
+hash kustomize &>/dev/null || { 
+  curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases/latest |\
+  grep browser_download |\
+  grep linux |\
+  cut -d '"' -f 4 |\
+  xargs curl -O -L
+  mv kustomize_*_linux_amd64 kustomize
+  chmod u+x ./kustomize
+  sudo mv ./kustomize /usr/local/bin/
+}
+hash kustomize &>/dev/null || { echo "kustomize not found! See https://github.com/kubernetes-sigs/kustomize/blob/master/docs/INSTALL.md"; exit 1; }
 
 hash gsutil || {
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
@@ -16,6 +41,10 @@ hash gsutil || {
     sudo apt-get update && sudo apt-get install -y google-cloud-sdk
     gcloud auth activate-service-account --key-file=tools/FormulaDB-storage-full.json
 }
+
+# -------------------------------------------------------------------------
+# External dependency: obj storage
+# -------------------------------------------------------------------------
 
 if ! gcloud auth list|grep formuladb-static-assets; then
     gcloud auth activate-service-account --key-file $BASEDIR/FormulaDB-storage-full.json
@@ -35,7 +64,7 @@ gsutil -m rsync -r fe/img gs://formuladb-static-assets/$ORGANIZ_NAME/formuladb/i
 gsutil -m rsync -r fe/icons gs://formuladb-static-assets/$ORGANIZ_NAME/formuladb/icons
 
 curl -L -O https://github.com/elastic/apm-agent-rum-js/releases/latest/download/elastic-apm-rum.umd.min.js
-gsutil -m rsync elastic-apm-rum.umd.min.js gs://formuladb-static-assets/$ORGANIZ_NAME/elastic-apm-rum.umd.min.js
+gsutil cp elastic-apm-rum.umd.min.js gs://formuladb-static-assets/$ORGANIZ_NAME/elastic-apm-rum.umd.min.js
 
 # -------------------------------------------------------------------------
 # External dependency: Elastic stack
