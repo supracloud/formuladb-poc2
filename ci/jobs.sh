d3992693504b42b9b82b7372820d259a38c7c2e0
Laurentiu Soica <laurentiu.soica@formuladb.io>
2019-09-24 21:03:50 +0300

    update ci jobs as LB was removed

--- ci/jobs.sh
+++ ci/jobs.sh
@@ -26,7 +26,7 @@ function build_images_and_deploy {
 
     bash $BASEDIR/prepare-organization.sh "$NAMESPACE" "$SKAFFOLD_PROFILE"
     skaffold -n $NAMESPACE run -p $SKAFFOLD_PROFILE
-    while ! kubectl -n $NAMESPACE get pods | grep 'lb-'; do sleep 1; done
+    while ! kubectl -n $NAMESPACE get pods | grep 'be-'; do sleep 1; done
     while ! kubectl -n $NAMESPACE get pods | grep 'db-.*Running'; do sleep 1; done
     POD=`kubectl -n $NAMESPACE get pod -l service=db -o jsonpath='{.items[0].metadata.name}'`
     nc -z localhost 5432 || kubectl -n $NAMESPACE port-forward $POD 5432:5432 &
@@ -57,10 +57,10 @@ function test_e2e {
     ORGANIZ_NAME=$1
     if [ -z "ORGANIZ_NAME" ]; then echo "pls provide ORGANIZ_NAME"; exit 1; fi
 
-    POD=`kubectl -n $ORGANIZ_NAME get pod -l service=lb -o jsonpath='{.items[0].metadata.name}'`
-    nc -z localhost 8085 || kubectl -n $ORGANIZ_NAME port-forward $POD 8085:80 &
+    POD=`kubectl -n $ORGANIZ_NAME get pod -l service=be -o jsonpath='{.items[0].metadata.name}'`
+    nc -z localhost 8084 || kubectl -n $ORGANIZ_NAME port-forward $POD 8084:8084 &
     npm run webdriver-update
-    TARGET=headless npm run test:e2e -- --baseUrl='http://localhost:8085'
+    TARGET=headless npm run test:e2e -- --baseUrl='http://localhost:8084'
 }
 
 function e2e_dev_env {
