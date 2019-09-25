05914180cbf78b24479f359d67fc9da14509058a
acristu <alexandru.cristu@formuladb.io>
2019-09-25 14:16:20 +0300

    not service local apps anymore through live-server

--- serve.sh
+++ serve.sh
@@ -8,6 +8,12 @@ handleErr () {
 }
 trap handleErr ERR
 
+while true; do
+    sleep 10
+    gsutil -m rsync -r gs://formuladb-static-assets/tdev-${USER}/formuladb-internal/ apps/formuladb-internal/
+    gsutil -m rsync -r gs://formuladb-static-assets/tdev-${USER}/formuladb-examples/ apps/formuladb-examples/
+done &
+
 mkdir -p live-server-wwwroot
 cd live-server-wwwroot #optimize speed
 ../node_modules/.bin/live-server --wait=200 --port=8081 -V --no-browser \
@@ -15,7 +21,4 @@ cd live-server-wwwroot #optimize speed
     --mount=/formuladb/:./../dist-fe/ \
     --mount=/formuladb/img/:./../fe/img/ \
     --mount=/formuladb/frmdb-runtime-utils.js:./../fe/js/frmdb-runtime-utils.js \
-    --mount=/formuladb-examples/:./../apps/formuladb-examples/ \
-    --mount=/formuladb-internal/:./../apps/formuladb-internal/ \
-    --mount=/:./../apps/formuladb-internal/formuladb.io/ \
     --proxy=/:http://localhost:8085/ \
