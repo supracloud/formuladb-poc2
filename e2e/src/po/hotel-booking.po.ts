af52488ee48cb357a6b7a8270db560504f0f3215
acristu <alexandru.cristu@formuladb.io>
2019-10-01 08:32:35 +0300

    added e2e for data binding highlight

--- e2e/src/po/hotel-booking.po.ts
+++ e2e/src/po/hotel-booking.po.ts
@@ -1,8 +1,8 @@
-import {browser, element, by, ExpectedConditions, ElementArrayFinder, ElementFinder} from 'protractor';
-import {Room, Booking} from '@test/hotel-booking/metadata';
+import { browser, element, by, ExpectedConditions, ElementArrayFinder, ElementFinder } from 'protractor';
+import { Room, Booking } from '@test/hotel-booking/metadata';
 
 export class HotelBooking {
- 
+
   async navigateToHome() {
     await browser.get('formuladb-editor/editor.html#/formuladb-examples/hotel-booking/index.html');
   }
@@ -55,17 +55,62 @@ export class HotelBooking {
   async byCss(selector: string) {
     await browser.switchTo().defaultContent();
     return await element(by.css(selector));
-  }  
+  }
   async allByCss(selector: string) {
     await browser.switchTo().defaultContent();
     return await element.all(by.css(selector));
+  }
+
+  async waitForSelectorInIframe(selector: string) {
+    let EC = ExpectedConditions;
+
+    await browser.switchTo().defaultContent();
+
+    // wait for iframe to be loaded
+    await browser.wait(EC.presenceOf(element(by.css('iframe'))), 21000);
+    await browser.switchTo().frame(0);
+    // wait for the document inside the iframe to be loaded
+    await browser.wait(EC.presenceOf(element(by.css(selector))), 22000);
+  }
+  async byCssInFrame(selector: string) {
+    await this.waitForSelectorInIframe(selector);
+    return element(by.css(selector));
+  }
+  async allByCssInFrame(selector: string) {
+    await this.waitForSelectorInIframe(selector);
+    return await element.all(by.css(selector));
+  }
+
+  /** NOT WORKING */
+  async scrollIntoView(elem) {
+    await browser.executeScript(function (el) {
+      arguments[0].scrollIntoView();
+    }, elem);
+  }
+
+  async clickWithJs(elem) {
+    await browser.executeScript(function (el) {
+      arguments[0].click();
+    }, elem);
+  }
+
+  async scrollIframe(scrollHeight: number) {
+    let EC = ExpectedConditions;
+    await browser.switchTo().defaultContent();
+    await browser.wait(EC.presenceOf(element(by.css('iframe'))), 7100);
+
+    await browser.executeScript(function (iframeEl, scrollHeight) {
+      let iframe = arguments[0];
+      let h = arguments[1];
+      iframe.contentWindow.scrollTo(0, h);
+    }, element(by.css('iframe')), scrollHeight);
   }
 
   async getTables() {
     // switch back to page content
     await browser.switchTo().defaultContent();
     let menuItems: Array<ElementFinder> = await element.all(by.css('[data-frmdb-value="$frmdb.tables[]._id"]'));
-    
+
     let tables: Array<string> = [];
     // Using getAttribute('innerText') hack to get the link text, as explained here https://stackoverflow.com/questions/20888592/gettext-method-of-selenium-chrome-driver-sometimes-returns-an-empty-string
     for (var i = 0; i < menuItems.length; i++) {
@@ -74,12 +119,12 @@ export class HotelBooking {
 
     return tables;
   }
-  
+
   async getPages() {
     // switch back to page content
     await browser.switchTo().defaultContent();
     let menuItems: Array<ElementFinder> = await element.all(by.css('[data-frmdb-value="$frmdb.pages[].name"]'));
-    
+
     let tables: Array<string> = [];
     // Using getAttribute('innerText') hack to get the link text, as explained here https://stackoverflow.com/questions/20888592/gettext-method-of-selenium-chrome-driver-sometimes-returns-an-empty-string
     for (var i = 0; i < menuItems.length; i++) {
@@ -102,7 +147,7 @@ export class HotelBooking {
     // document.querySelector('#data-grid > frmdb-data-grid').shadowRoot.querySelectorAll('#myGrid div.ag-row[row-index="0"] div.ag-cell')
     // let firstRowCells: Array<any> = await element.all(by.js(() => {document.querySelector('#data-grid > frmdb-data-grid').shadowRoot.querySelectorAll('#myGrid div.ag-row[row-index="0"] div.ag-cell')}));
     let firstRowCells: Array<any> = [];
-    
+
     let firstRoomData: { id: string, value: string }[] = [];
     for (var i = 0; i < firstRowCells.length; i++) {
       firstRoomData.push({
--- e2e/src/specs/hotel-booking.spec.ts
+++ e2e/src/specs/hotel-booking.spec.ts
@@ -9,13 +9,15 @@ import { HotelBooking } from '../po/hotel-booking.po';
 import { browser } from 'protractor';
 import * as e2e_utils from "../utils";
 
-var messages = [ '<speak>Welcome to the Hotel Booking app template. As an admin you can customize the app. On the left side pane there are the available data tables<break time="1s"/></speak>',
-                 '<speak>For the Hotel Booking app you will already find a few predefined Tables like RoomType, Room or Booking<break time="1s"/></speak>',
-                 '<speak>Also there are the predefined Pages like the home page, about page, gallery page, contact page<break time="1s"/></speak>',
-                 '<speak>You will be able to make quick cosmetic/branding changes like change the color palette</speak>',
+var messages = [ '<speak>Welcome to the Hotel Booking app template draft intro video. As an admin you can customize the app. On the left side pane there are the available data tables<break time="1s"/></speak>',
+                 '<speak>For the Hotel Booking app you will find a few predefined Tables like RoomType, Room or Booking<break time="1s"/></speak>',
+                 '<speak>You will also find the predefined Pages like the home page, about page, gallery page, contact page<break time="1s"/></speak>',
+                 '<speak>You can get started quickly with very simple cosmetic/branding changes like change the color palette</speak>',
                  '<speak>and the website language<break time="1s"/></speak>',
-                 '<speak>You will be able to manage dynamic content and logic by mapping Table data to Page elements, for example we can display a list of Room Types as a list of cards on the home page.<break time="1s"/></speak>',
-                 '<speak>Please follow formuladb.io and our social media for news about the official launch.<break time="1s"/></speak>',
+                 '<speak>You can create more powerful customizations by binding Page elements to data from Table Records<break time="1s"/></speak>',
+                 '<speak>For example you can display the room types as a list of cards<break time="1s"/></speak>',
+                 '<speak>You can use Formulas to perform computations<break time="1s"/></speak>',
+                 '<speak>Please follow formuladb.io for more details and news about the official launch.<break time="1s"/></speak>',
 ];
 var durations = new Array(messages.length);
 
@@ -66,7 +68,7 @@ describe('hotel-booking view mode testing', () => {
     expect(foundTables).toEqual(true);
   });
 
-  it('should load the room types list', async () => {
+  xit('should load the room types list', async () => {
     await e2e_utils.handle_generic_action(durations[action_index++]);
     // check that room types list is correctly displayed; verify the first row against the e2e data
     let roomTypes: { id: string, value: string }[] = await hotelBooking.getFirstRoomTypeData();
@@ -74,7 +76,7 @@ describe('hotel-booking view mode testing', () => {
     //expect(['RoomType', 'Room', 'Booking'].sort().toString() == bookingTables.sort().toString());
   });
 
-  it('should load the page list: index.html, about.html, contact.html', async () => {
+  xit('should load the page list: index.html, about.html, contact.html', async () => {
     await e2e_utils.handle_element_click(await hotelBooking.getPagesDropdown(), durations[action_index++]);
     // check that at least hotel booking tables ('RoomType', 'Room', 'Booking') are displayed in the left navbar
     let foundPages = await e2e_utils.retryUntilTrueOrRetryLimitReached(async () => {
@@ -85,37 +87,73 @@ describe('hotel-booking view mode testing', () => {
     expect(foundPages).toEqual(true);
   });
 
-  it('should change theme color', async () => {
+  xit('should change theme color', async () => {
     await e2e_utils.handle_element_click(await hotelBooking.byCss('#frmdb-editor-color-palette-select'), durations[action_index++]);
     let colors = await hotelBooking.allByCss('[aria-labelledby="frmdb-editor-color-palette-select"] .dropdown-item');
+    await browser.sleep(550);
     await colors[1].click();
     await browser.sleep(200);
     let icon = await hotelBooking.getLogoIcon();
     let color = await icon.getCssValue('color');
     expect(color).toEqual('rgba(255, 0, 0, 1)');
-    await browser.sleep(1000);
+    await browser.sleep(2200);
 
     await (await hotelBooking.byCss('#frmdb-editor-color-palette-select')).click();
     colors = await hotelBooking.allByCss('[aria-labelledby="frmdb-editor-color-palette-select"] .dropdown-item');
     await colors[0].click();
-    await browser.sleep(500);
+    await browser.sleep(1500);
     icon = await hotelBooking.getLogoIcon();
     color = await icon.getCssValue('color');
     expect(color).toEqual('rgba(243, 195, 0, 1)');
   });
 
-  it('should change language', async () => {
+  xit('should change language', async () => {
     await e2e_utils.handle_element_click(await hotelBooking.byCss('#frmdb-editor-i18n-select'), durations[action_index++]);
     let languages = await hotelBooking.allByCss('[aria-labelledby="frmdb-editor-i18n-select"] .dropdown-item');
+    await browser.sleep(550);
     await languages[1].click();
-    await browser.sleep(1000);
+    await browser.sleep(2200);
     expect(await hotelBooking.getPageTitle()).toEqual('Détends ton esprit');
 
     await (await hotelBooking.byCss('#frmdb-editor-i18n-select')).click();
     languages = await hotelBooking.allByCss('[aria-labelledby="frmdb-editor-i18n-select"] .dropdown-item');
     await languages[0].click();
-    await browser.sleep(1000);
+    await browser.sleep(2200);
     expect(await hotelBooking.getPageTitle()).toEqual('Relax Your Mind');
+  });
+
+  it('should highlight column for data binding', async () => {
+    await e2e_utils.handle_generic_action(durations[action_index++]);
+    await hotelBooking.scrollIframe(350);
+    let arivalDateFormEl = await hotelBooking.byCssInFrame('[data-frmdb-value="::start_date"]');
+    await browser.sleep(1000);
+    let departureDateFormEl = await hotelBooking.byCssInFrame('[data-frmdb-value="::end_date"]');
+    await hotelBooking.clickWithJs(arivalDateFormEl);
+    await browser.sleep(150);
+    await hotelBooking.clickWithJs(departureDateFormEl);
+    await browser.sleep(150);
+    await hotelBooking.clickWithJs(arivalDateFormEl);
+
+    await browser.sleep(1050);
+    await hotelBooking.clickWithJs(departureDateFormEl);
+    //TODO check background color of column in the data grid
+
+    await browser.sleep(1050);
+    //TODO check background color of column in the data grid
+    let nbAdultsFormEl = await hotelBooking.byCssInFrame('[data-frmdb-value="::nb_adults"]');
+    await hotelBooking.clickWithJs(nbAdultsFormEl);
+
+    await browser.sleep(1050);
+    //TODO check background color of column in the data grid
+    let nbChildrenFormEl = await hotelBooking.byCssInFrame('[data-frmdb-value="::nb_children"]');
+    await hotelBooking.clickWithJs(nbChildrenFormEl);
+    await browser.sleep(1050);
+    //TODO check background color of column in the data grid
+  });
+
+  xit('should work with data binding for cards', async () => {
+    await e2e_utils.handle_generic_action(durations[action_index++]);
+    await hotelBooking.scrollIframe(350);
   });
 
   it('Should end recording and cleanup', async () => {
