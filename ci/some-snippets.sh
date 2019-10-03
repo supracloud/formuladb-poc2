a2eadbf05d5b47bb65287236570d5313cd0eae4d
acristu <alexandru.cristu@formuladb.io>
2019-10-03 14:53:01 +0300

    scripts for git storage

--- ci/some-snippets.sh
+++ ci/some-snippets.sh
@@ -3,3 +3,18 @@ GIT_SSH_COMMAND="ssh -i /mnt/d/code/metawiz/febe/ssh/frmdb.id_rsa" git clone git
 GIT_SSH_COMMAND="ssh -i /mnt/d/code/metawiz/febe/ssh/frmdb.id_rsa" git clone git@gitlab.com:formuladb-apps/Hotel_Booking----obj.git
 
 GIT_SSH_COMMAND="ssh -i /mnt/d/code/metawiz/febe/ssh/frmdb.id_rsa" git clone git@gitlab.com:formuladb-themes/royal.git
+
+GIT_SSH_COMMAND="ssh -i /mnt/d/code/metawiz/febe/ssh/frmdb.id_rsa" git clone git@gitlab.com:formuladb-internal-apps/formuladb.io.git
+GIT_SSH_COMMAND="ssh -i /mnt/d/code/metawiz/febe/ssh/frmdb.id_rsa" git clone git@gitlab.com:formuladb-internal-apps/formuladb.io----db.git
+GIT_SSH_COMMAND="ssh -i /mnt/d/code/metawiz/febe/ssh/frmdb.id_rsa" git clone git@gitlab.com:formuladb-internal-apps/formuladb.io----obj.git
+
+
+## Create new App ##############
+FRMDB_APP_NAME=$1
+if [ -z "$FRMDB_APP_NAME" ]; then echo "Usage: create-app.sh FRMDB_ENV_NAME FRMDB_TENANT_NAME FRMDB_APP_NAME"; exit 1; fi
+
+for appName in "${FRMDB_APP_NAME}" "${FRMDB_APP_NAME}----db" "${FRMDB_APP_NAME}----obj"; do
+    curl -v --request POST --header 'PRIVATE-TOKEN: RER-gkXZCCi8irBNsUgL' --header "Content-Type: application/json" \
+        --data '{"path": "formuladb-apps/'${appName}'"}' \
+        'https://gitlab.com/api/v4/projects'
+done
--- scripts/create-app-for-env.sh
+++ scripts/create-app-for-env.sh
@@ -9,16 +9,34 @@ export BASEDIR=`dirname $0`
 
 export GIT_SSH_COMMAND="ssh -i ${BASEDIR}/../ssh/frmdb.id_rsa"
 mkdir -p ${FRMDB_TENANT_NAME}
-git clone --branch "${FRMDB_ENV_NAME}" --single-branch \
+
+if git ls-remote --heads git@gitlab.com:${FRMDB_TENANT_NAME}/${FRMDB_APP_NAME}.git "${FRMDB_TENANT_NAME}" | grep "${FRMDB_TENANT_NAME}"; then
+    echo "app ${FRMDB_TENANT_NAME}/${FRMDB_APP_NAME} already exists for this env ${FRMDB_TENANT_NAME}";
+    exit 1
+fi
+
+git clone --branch master --single-branch \
     git@gitlab.com:${FRMDB_TENANT_NAME}/${FRMDB_APP_NAME}.git ${FRMDB_TENANT_NAME}/${FRMDB_APP_NAME}
-git clone --branch "${FRMDB_ENV_NAME}" --single-branch \
+git checkout -b "${FRMDB_TENANT_NAME}"
+git push
+git clone --branch master --single-branch \
     git@gitlab.com:${FRMDB_TENANT_NAME}/${FRMDB_APP_NAME}.git ${FRMDB_TENANT_NAME}/${FRMDB_APP_NAME}----db
-git clone --branch "${FRMDB_ENV_NAME}" --single-branch \
+git checkout -b "${FRMDB_TENANT_NAME}"
+git push
+git clone --branch master --single-branch \
     git@gitlab.com:${FRMDB_TENANT_NAME}/${FRMDB_APP_NAME}.git ${FRMDB_TENANT_NAME}/${FRMDB_APP_NAME}----obj
+git checkout -b "${FRMDB_TENANT_NAME}"
+git push
 
 mkdir -p formuladb-themes
 THEME_NAME=`cat ${FRMDB_TENANT_NAME}/${FRMDB_APP_NAME}/app.yaml | grep theme_name | cut -d ':' -f2`
+if git ls-remote --heads git@gitlab.com:formuladb-themes/${THEME_NAME}.git "${FRMDB_TENANT_NAME}" | grep "${FRMDB_TENANT_NAME}"; then
+    echo "theme ${THEME_NAME} already exists for this env ${FRMDB_TENANT_NAME}";
+    exit 2
+fi
 if [[ -n "$THEME_NAME" ]]; then
-    git clone --branch "${FRMDB_ENV_NAME}" --single-branch \
+    git clone --branch master --single-branch \
         git@gitlab.com:formuladb-themes/${THEME_NAME}.git formuladb-themes/${THEME_NAME}
+    git checkout -b "${FRMDB_TENANT_NAME}"
+    git push
 fi
