ee8e155c0a488abda95998507d10086f1c39868c
Laurentiu Soica <laurentiu.soica@formuladb.io>
2019-10-27 22:19:45 +0200

    remove unneeded package

--- be/src/config/env-manager.ts
+++ be/src/config/env-manager.ts
@@ -5,7 +5,6 @@
 
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-const retry = require('async-retry')
 const fetch = require('node-fetch')
 
 async function delay(ms) {
