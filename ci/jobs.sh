c99299430c7f5537bfd189942104b21dda6d1d3b
Alex Cristu <alexandru.cristu@formuladb.io>
2020-01-16 18:48:52 +0200

    ciii

--- ci/jobs.sh
+++ ci/jobs.sh
@@ -121,7 +121,7 @@ function cleanup {
     find /home/gitlab-runner/cache/ -type f -mmin +60 -delete
     # cleanup registry: BE development images in febe project
     bash ./ci/cleanup-docker-registry.sh mfDqKQ6zwhZaszaNpUys 4245551 398919 7
-    namespacesToDelete=`kubectl get namespaces|egrep '[0-9a-f]{40} .*Active.*  [2-9][0-9]d$'|egrep -o "[0-9a-f]{40}" || ''`
+    namespacesToDelete=`kubectl get namespaces|egrep '[0-9a-f]{40} .*Active.*  [2-9][0-9]d$'|egrep -o "[0-9a-f]{40}" || true`
     if [[ -n "$namespacesToDelete" ]]; then kubectl delete namespace $namespacesToDelete; fi
 }
 
