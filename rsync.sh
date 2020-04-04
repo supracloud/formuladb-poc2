fc42d85fd5ef32d9d10e39aa85fa661843db9741
Alex Cristu <alexandru.cristu@formuladb.io>
2020-04-04 22:33:10 +0300

    fixes

--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,3 @@
-bash ci/kubectlrsync.sh -crq --exclude=.git be:/wwwroot/git/formuladb-env/frmdb-apps/ formuladb-env/frmdb-apps/
+bash ci/kubectlrsync.sh -crq --exclude=.git be:/wwwroot/git/formuladb-env/ formuladb-env/
 bash ci/kubectlrsync.sh -crq --exclude=.git be:/wwwroot/git/formuladb-env/db/ formuladb-env/db/
 bash ci/kubectlrsync.sh -crq --exclude=.git be:/wwwroot/git/formuladb-env/static/ formuladb-env/static/
