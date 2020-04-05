3d5ca0839bdb129c9d1a88ef6fb6617d3e177c30
Alex Cristu <alexandru.cristu@formuladb.io>
2020-04-05 11:38:22 +0300

    fix e2e call

--- ci/jobs.sh
+++ ci/jobs.sh
@@ -103,9 +103,8 @@ function test_e2e {
     # nc -z localhost 8084 || kubectl -n $FRMDB_ENV_NAME port-forward $POD 8084:3000 &
     while ! curl $URL/formuladb-api/hotel-booking/schema | grep 'Room_Type'; do sleep 2; done
 
-    TARGET=headless npm test -- --specs \
-        tsc-out/docs/1-Intro/6-editor-tables-list.e2e.js \
-        tsc-out/docs/0-GetStarted/7-list-of-pages.e2e.js
+    TARGET=headless npm test -- -- --specs \
+        "tsc-out/docs/1-Intro/6-editor-tables-list.e2e.js,tsc-out/docs/0-GetStarted/7-list-of-pages.e2e.js"
 }
 
 function e2e_dev_env {
