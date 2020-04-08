5bd5f8c35ef45e5835f2cd4c5d03ffdc814d8821
Alex Cristu <alexandru.cristu@formuladb.io>
2020-04-08 11:38:01 +0300

    ci

--- ci/jobs.sh
+++ ci/jobs.sh
@@ -51,6 +51,7 @@ function build_images_and_deploy_dev {
 function test_postgres {
     # POD=`kubectl -n $FRMDB_ENV_NAME get pod -l service=db -o jsonpath='{.items[0].metadata.name}'`
     # nc -z localhost 5432 || kubectl -n $FRMDB_ENV_NAME port-forward $POD 5432:5432 &
+    docker ps|grep "${FRMDB_ENV_NAME}-pg" |cut -d' ' -f1|xargs docker rm -f || true
     while nc -z localhost 5434; do echo "port 5434 is busy, waiting..."; sleep 2; done
     docker run --name "${FRMDB_ENV_NAME}-pg" -p5434:5432 -e POSTGRES_PASSWORD=postgres postgres:11 &
     while ! nc -z localhost 5434; do sleep 1; done
@@ -62,6 +63,7 @@ function test_stress {
     # npm test -- core/src/frmdb_engine.stress.spec.ts
     # POD=`kubectl -n $FRMDB_ENV_NAME get pod -l service=db -o jsonpath='{.items[0].metadata.name}'`
     # nc -z localhost 5432 || kubectl -n $FRMDB_ENV_NAME port-forward $POD 5432:5432 &
+    docker ps|grep "${FRMDB_ENV_NAME}-stress" |cut -d' ' -f1|xargs docker rm -f || true
     while nc -z localhost 5435; do echo "port 5435 is busy, waiting..."; sleep 2; done
     docker run --name "${FRMDB_ENV_NAME}-stress" -p5435:5432 -e POSTGRES_PASSWORD=postgres postgres:11 &
     while ! nc -z localhost 5435; do sleep 1; done
