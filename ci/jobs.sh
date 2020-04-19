7a00c9defef73ffea229427846c9099d886287c0
Laurentiu Soica <laurentiu@soica.ro>
2020-04-19 13:20:45 +0000

    add chrome

--- ci/jobs.sh
+++ ci/jobs.sh
@@ -67,6 +67,8 @@ function test_stress {
 
 function test_e2e {
     set -x
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    apt-get install -y ./google-chrome-stable_current_amd64.deb
 
     if [ ! -d "formuladb-e2e" ]; then
         chmod og-rwx ssh
